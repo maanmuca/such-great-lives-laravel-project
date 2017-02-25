4e51ca972037528c688b70f489b550533992590a
Paolapps <potisavi@gmail.com>
2017-02-26 01:41:50 +1100

    fotos

--- resources/views/pages/albums.blade.php
+++ resources/views/pages/albums.blade.php
@@ -16,25 +16,17 @@
                 @foreach($albums['albums'] as $album)
                     <a href="photos/{{$album['id']}}">
                         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 nopadding">
-                            <div class="hovereffect" style="background-image: url({{$album['photos'][0]['images'][1]['source']}})">
-                                <img src="" />
-                                <div class="overlay">
-                                    <p>
+                            <div class="hovereffect" >
+                                <div class="overlay" style="background-image: url({{$album['photos'][0]['images'][1]['source']}})">
+                                   
                                    <h5>{{$album['name']}}</h5>
-                                    <p>Share on</p>
-                                    </p>
+                                   <p>Share on</p>
+                                   <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fsuchgreatlives%2F&amp;src=sdkpreparse">
+                                       <i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                   
                                 </div>
                             </div>
-                            <div class="buttonsAlbum text-center">
-                                <div class="btn btn-primary" data-href="https://www.facebook.com/suchgreatlives/" 
-                                     data-layout="button_count" data-toggle="tooltip" title="Share on Facebook" 
-                                     data-size="large" data-mobile-iframe="true">
-                                    <a class="fb-xfbml-parse-ignore" target="_blank" 
-                                     href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fsuchgreatlives%2F&amp;src=sdkpreparse">
-                                        <i class="fa fa-facebook myFacebookShareButton" aria-hidden="true"></i></a>
-                                </div>
-                                
-                            </div>
+                            
                         </div>
                     </a>
                 @endforeach
