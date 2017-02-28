8893649fd3ffcf09ac992cd89d82379f3ae16f80
Paolapps <potisavi@gmail.com>
2017-03-01 09:49:26 +1100

    album

--- resources/views/pages/albums.blade.php
+++ resources/views/pages/albums.blade.php
@@ -17,18 +17,22 @@
                     <a href="photos/{{$album['id']}}">
                         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 nopadding">
                             <div class="hovereffect">
-                                <img src="{{$album['photos'][0]['images'][1]['source']}}" height="250" width="250"/>
+                                <img src="{{$album['photos'][0]['images'][1]['source']}}"/>
                                 <div class="overlay">
                                     <p>
-                                   <h5>{{$album['name']}}</h5>
+                                    <h5>{{$album['name']}}</h5>
                                     <p>Share on</p>
                                     </p>
                                 </div>
                             </div>
                             <div class="buttonsAlbum text-center">
-                                <div class="btn btn-primary" data-href="https://www.facebook.com/suchgreatlives/" data-layout="button_count" data-toggle="tooltip" title="Share on Facebook" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fsuchgreatlives%2F&amp;src=sdkpreparse"><i class="fa fa-facebook myFacebookShareButton" aria-hidden="true"></i></a></div>
-                                <a class="btn btn-primary" href="#topAlbums" data-toggle="tooltip" title="Back to the top"><i class="fa fa-arrow-up" aria-hidden="true"></i></a>
-                                <a class="btn btn-primary" href="{{url('/')}}" data-toggle="tooltip" title="Back to the home Page"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+                                <div class="btn btn-primary" data-href="https://www.facebook.com/suchgreatlives/" 
+                                     data-layout="button_count" data-toggle="tooltip" title="Share on Facebook" 
+                                     data-size="large" data-mobile-iframe="true">
+                                    <a class="fb-xfbml-parse-ignore" target="_blank" 
+                                    href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fsuchgreatlives%2F&amp;src=sdkpreparse">
+                                        <i class="fa fa-facebook myFacebookShareButton" aria-hidden="true"></i></a>
+                                </div>
                             </div>
                         </div>
                     </a>
